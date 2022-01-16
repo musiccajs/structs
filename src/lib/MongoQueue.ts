10be4f82822363446ccb3945f63b20d301b1e893
ThEditor <theditormcpe@gmail.com>
2022-01-16 11:46:25 +0530

    fix: mongoqueue now reconstructs media

--- src/lib/MongoQueue.ts
+++ src/lib/MongoQueue.ts
@@ -19,20 +19,27 @@ export default class MongoQueue extends Queue {
 
   public id: string;
 
-  constructor(model: Model<IQueue>, manager: QueueManager<Queue>, id: string) {
+  constructor(manager: QueueManager<Queue>, id: string, { model }: { model: Model<IQueue> }) {
     super(manager, id);
     this.id = id;
-    // eslint-disable-next-line new-cap
-    new model({
-      _id: this.id,
-      list: []
-    }).save();
     this.db = model;
+    this.connect(model);
+  }
+
+  private async connect(model: Model<IQueue>) {
+    const doc = await this.db.findById(this.id);
+    // eslint-disable-next-line new-cap
+    const mod = new model({
+      _id: this.id
+    });
+    mod.isNew = !doc;
+    mod.markModified('list');
+    await mod.save();
   }
 
   public async all() {
-    const a = await this.db.findById(this.id);
-    return a?.list ?? [];
+    const a = (await this.db.findById(this.id))?.list;
+    return this.reconstructMedia(a);
   }
 
   public async add<T extends Media | Media[] = Media>(media: T, position?: number) {
@@ -43,22 +50,21 @@ export default class MongoQueue extends Queue {
     else coll?.list.push(...wrap);
 
     coll?.markModified('list');
-    coll?.save();
+    await coll?.save();
 
     return media;
   }
 
   public async get(position: number) {
-    const a = await this.db.findById(this.id);
-    return a?.list[position];
+    return this.reconstructMedia((await this.db.findById(this.id))?.list[position])[0];
   }
 
   public async remove(position: number) {
     const a = await this.db.findById(this.id);
     const x = a?.list.splice(position, 1)[0];
     a?.markModified('list');
-    a?.save();
-    return x;
+    await a?.save();
+    return this.reconstructMedia(x)[0];
   }
 
   public async clear() {
@@ -66,11 +72,23 @@ export default class MongoQueue extends Queue {
     const x = a as IQueue;
     x.list = [];
     a?.markModified('list');
-    a?.save();
+    await a?.save();
   }
 
   public async indexOf(media: Media) {
     const a = await this.db.findById(this.id);
     return a?.list.indexOf(media) ?? -1;
+  }
+
+  private reconstructMedia(media: Media | Media[] | undefined) {
+    const wrap = Array.isArray(media) ? media : [media];
+    const reconstructed: Media[] = [];
+    wrap.forEach((m) => {
+      if (!m) return;
+      const extractor = this.manager.client.extractors.get(m.extractor.id);
+      if (!extractor) return;
+      reconstructed.push(new Media(extractor, m.url, m.data, m.id));
+    });
+    return reconstructed;
   }
 }
